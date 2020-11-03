84e5eca705a951b0ee64f18d91ad530da71acebe
Faith1sGay <daniel.viola001@mymdc.net>
2020-11-03 11:14:14 -0500

    I AM SMART

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-class triangle {
-  constructor (height, width) {
-    this.height = height
-    this.width = width
-  }
-}
-
+const triangle = require('./triangle.js')
 const tri = new triangle(10, 17)
 console.log(`The area of this triangle is ${tri.height * tri.width / 2}.`)
---
+++ triangle.js
@@ -0,0 +1,7 @@
+class triangle {
+    constructor(height, width) {
+        this.height = height
+        this.width = width
+    }
+}
+module.exports = triangle
